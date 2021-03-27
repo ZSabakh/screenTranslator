1e5f3be841f60c98d29556db2bbb8ed0c6c94dc2
ZSabakh <zsabakhtarishvili@atu.edu>
2021-03-27 15:30:18 -0500

    Added OCR support for screen area

--- ScreenTranslate.py
+++ ScreenTranslate.py
@@ -1,8 +1,11 @@
 import keyboard
 import mouse
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 import pyautogui
 import time
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # TODO On CTRL+F6 track mouse location, on click save location and keep tracking while user moves to another location
 # TODO When user moves to another location and presses CTRL+F7 the new location is saved and then subtract values to get height and width.
@@ -10,24 +13,27 @@ import time
 while True:
     try:
         if keyboard.is_pressed('ctrl+F6'):
-            startPointCoordinates = [mouse.get_position()[0], mouse.get_position()[1]]
+            start_point_coordinates = [mouse.get_position()[0], mouse.get_position()[1]]
+
 
             while True:
                 if mouse.is_pressed():
-                    endPointCoordinates = [mouse.get_position()[0], mouse.get_position()[1]]
+                    end_point_coordinates = [mouse.get_position()[0], mouse.get_position()[1]]
                     break
                 time.sleep(0.002)
+            if (end_point_coordinates[0] < start_point_coordinates[0]):
+                start_point_coordinates[0], end_point_coordinates[0] = end_point_coordinates[0], start_point_coordinates[0]
+            if (end_point_coordinates[1] < start_point_coordinates[1]):
+                start_point_coordinates[1], end_point_coordinates[1] = end_point_coordinates[1], start_point_coordinates[1]
 
-            if(endPointCoordinates[0] < startPointCoordinates[0]):
-                startPointCoordinates[0], endPointCoordinates[0] = endPointCoordinates[0], startPointCoordinates[0]
-            if(endPointCoordinates[1] < startPointCoordinates[1]):
-                startPointCoordinates[1], endPointCoordinates[1] = endPointCoordinates[1], startPointCoordinates[1]
-
-            image = ImageGrab.grab(bbox=(
-                startPointCoordinates[0], startPointCoordinates[1], endPointCoordinates[0], endPointCoordinates[1]))
-            image.save('sc.png')
-
+            while True:
+                image = ImageGrab.grab(bbox=(
+                    start_point_coordinates[0], start_point_coordinates[1], end_point_coordinates[0], end_point_coordinates[1]))
+                image.save('sc.png')
+                print(pytesseract.image_to_string(Image.open('sc.png')))
+                time.sleep(0.1)
         time.sleep(0.002)
+
     except Exception as e:
         print(e)
         break
