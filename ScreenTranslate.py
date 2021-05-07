2be7afe8eee5d4954a19884256ca42af1fc43c6f
ZSabakh <zsabakhtarishvili@atu.edu>
2021-05-07 18:51:21 -0500

    Added crosshairs for better UI

--- ScreenTranslate.py
+++ ScreenTranslate.py
@@ -16,6 +16,16 @@ root.overrideredirect(False)
 root.wm_attributes()
 root.wm_attributes("-topmost", True)
 
+crossStart = tk.Tk()
+crossStart.overrideredirect(True)
+crossStart.wm_attributes("-topmost", True)
+crossStart.wm_attributes("-transparentcolor", 'grey')
+
+crossEnd = tk.Tk()
+crossEnd.overrideredirect(True)
+crossEnd.wm_attributes("-topmost", True)
+crossEnd.wm_attributes("-transparentcolor", 'grey')
+
 def change_label(label):
     def scanImage():
         image = ImageGrab.grab(bbox=(
@@ -25,7 +35,7 @@ def change_label(label):
         gray = cv2.cvtColor(processed_image, cv2.COLOR_BGR2GRAY)
         thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
         thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
-
+        cv2.imshow("thresh", thresh)
         scanned_text = pytesseract.image_to_string(thresh, lang='ita', config='--psm 6').replace("", "").replace("\n", " ")
         translated_text = ""
         try:
@@ -47,8 +57,10 @@ while True:
             while True:
                 if mouse.is_pressed():
                     end_point_coordinates = [mouse.get_position()[0], mouse.get_position()[1]]
+
                     break
                 time.sleep(0.002)
+
             if (end_point_coordinates[0] < start_point_coordinates[0]):
                 start_point_coordinates[0], end_point_coordinates[0] = end_point_coordinates[0], \
                                                                        start_point_coordinates[0]
@@ -57,10 +69,24 @@ while True:
                                                                        start_point_coordinates[1]
             root.geometry(f"+{250}+{250}")
 
+            crossStart.geometry(f"+{start_point_coordinates[0]}+{start_point_coordinates[1]}")
+            crossEnd.geometry(f"+{end_point_coordinates[0]}+{end_point_coordinates[1]}")
+
+            crossLabelStart = tk.Label(crossStart, font=('Times New Roman', '20'), fg='red', bg='gray')
+            crossLabelStart.config(text="+")
+            crossLabelStart.pack(expand=True)
+
+            crossLabelEnd = tk.Label(crossStart, font=('Times New Roman', '20'), fg='red', bg='gray')
+            crossLabelEnd.config(text="+")
+            crossLabelEnd.pack(expand=True)
+
             while True:
+
                 label = tk.Label(root, font=('Times New Roman', '20'), fg='black', bg='white')
                 label.pack(expand=True)
                 change_label(label)
+                crossStart.mainloop()
+                crossEnd.mainloop()
                 root.mainloop()
 
         time.sleep(0.002)
